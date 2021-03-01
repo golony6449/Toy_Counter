b2da0828ccb8d7c3885b3970113f3f9119f0fc8f
Parkseongheum <ab6449@gmail.com>
2021-03-01 20:12:12 +0900

    init Project

---
+++ src/main/java/dev/golony/toy/counter/CounterApplication.java
@@ -0,0 +1,13 @@
+package dev.golony.toy.counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterApplication.class, args);
+	}
+
+}
